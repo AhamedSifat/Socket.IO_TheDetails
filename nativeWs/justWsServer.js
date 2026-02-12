37de1d8ead8f96514b523090bed8fa9f9ae1ebc7
sifat1234 <ahahmedsifat111@gmail.com>
2026-02-12 23:55:46 +0600

    implement WebSocket client and server connection handling

--- nativeWs/justWsServer.js
+++ nativeWs/justWsServer.js
@@ -7,6 +7,19 @@ const server = http.createServer((req, res) => {
 
 const wss = new WebSocketServer({ server });
 
+wss.on('headers', (headers, req) => {
+  console.log(headers);
+});
+
+wss.on('connection', (ws, req) => {
+  console.log('New WebSocket connection');
+  ws.on('message', (message) => {
+    console.log('Received message from client:', message.toString());
+  });
+
+  ws.send('Welcome to the WebSocket server!');
+});
+
 server.listen(8080, () => {
   console.log('Server is listening on port 8080');
 });
