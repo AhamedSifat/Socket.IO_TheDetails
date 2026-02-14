f21b978d4e2c0661cd251fe5d6bec5efa8dc9ee2
sifat1234 <ahahmedsifat111@gmail.com>
2026-02-14 17:53:56 +0600

    feat: add initial Socket.IO chat application with HTML and CSS layout

---
+++ socketio201/namespaces/namespaces.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+app.use(express.static('public'));
+
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.emit('messageFromServer', { data: 'Welcome to the server!' });
+
+  socket.on('newMessageToServer', (message) => {
+    io.emit('newMessageToClients', { text: message.text });
+  });
+});
+
+httpServer.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
